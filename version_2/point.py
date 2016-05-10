b4352e6dbfb779f301afac1f5a3672fcae7c3878
vzaika <imressed@gmail.com>
2016-05-10 14:02:10 +0200

    add hashing func to point class

--- version_2/point.py
+++ version_2/point.py
@@ -2,7 +2,11 @@
 
 class Point(object):
 
-    def __init(self, id, x, y):
+    def __init__(self, id, x, y, edge_id):
         self.id = id
         self.x = x
         self.y = y
+        self.edge_id = edge_id
+
+    def __hash__(self):
+        return (self.x, self.y)
